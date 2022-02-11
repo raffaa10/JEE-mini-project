837d2ea3feab14cab7e4b0ae4f058c2ef939a355
Rafayel Azizyan <rafayel.azizyan@gmail.com>
2022-02-11 02:20:41 +0100

    Advanced backing beans - Displaying data - Navigation

---
+++ src/main/java/fr/epsi/jsf/CatalogItemFormBean.java
@@ -0,0 +1,52 @@
+package fr.epsi.jsf;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@SessionScoped
+@Named
+public class CatalogItemFormBean implements Serializable {
+	
+	private CatalogItem catalogItem = new CatalogItem();
+	
+	private List<CatalogItem> items = new ArrayList<>();
+	
+	public String addItem() {
+		// on initialise l'itemId, parce que on ne l'a pas dans le formulaire
+		long itemId = this.items.size() + 1;
+		this.items.add(new CatalogItem(itemId, this.catalogItem.getName(),
+				this.catalogItem.getManufacturer(),
+				this.catalogItem.getDescription(),
+				this.catalogItem.getAvailableDdate()
+				));
+		this.items.stream().forEach(item ->{
+			System.out.println(item.toString());
+		});
+		return "list?faces-redirect=true";		
+	}
+	
+		
+	public CatalogItem getCatalogItem() {
+		return catalogItem;
+	}
+
+	public void setCatalogItem(CatalogItem catalogItem) {
+		this.catalogItem = catalogItem;
+	}
+
+	public List<CatalogItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<CatalogItem> items) {
+		this.items = items;
+	}
+	
+	
+
+}
---
+++ src/main/webapp/resources/js/shake.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+
+document.addEventListener("DOMContentLoaded", function(event){
+	document.getElementById("item-form").style.animation = "shake .10s";
+});
